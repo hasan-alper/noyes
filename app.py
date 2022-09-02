310f235c6adee06297c4245d8d2c6c4e8cb8962b
Hasan Alper Aydın <hasanalper.aydin@gmail.com>
2022-09-02 13:19:35 +0300

    build basic flask app

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from joblib import load
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def predict():
+    if request.method == "GET":
+        return render_template("app.html")
+    else:
+        model = load("model/model.joblib")
+        print(request.form.to_dict(flat=False)["content"])
+        age = model.predict(request.form.to_dict(flat=False)["content"])[0][0]
+        type = model.predict(request.form.to_dict(flat=False)["content"])[0][1]
+        return render_template("app.html", age=age, type=type)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
